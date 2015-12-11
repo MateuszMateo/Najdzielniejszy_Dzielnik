0af751a3517fdbae4127b363e2bda5f53309d4e1
MateuszMateo <k.mateusz120@gmail.com>
2015-12-11 17:50:44 +0100

    Sortowanie wielokrotności dzielników

--- main.cpp
+++ main.cpp
@@ -2,39 +2,38 @@
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>
+#include <functional>
+
 using namespace std;
 
 int main()
 {
-
     int n;
     long long N=1;
     cin>>n;
     int tablica [n+1];
     for(int i=0;i<n;i++){
         cin>>tablica[i];
-
     }
     for(int i=0;i<n;i++){
         N=N*tablica[i];
     }
-    cout<<N<<endl;
 
     int P=sqrt(N);
-
     int Wielokrotnosci[P+1];
     int Dzielniki[P+1];
-    bool DzielnikiPF[P+1];
+    int Wielokrotnosci2[P+1];
 
     for(int i=0;i<=sqrt(N);i++){
         Wielokrotnosci[i]=0;
-        DzielnikiPF[i]=0;
+        Wielokrotnosci2[i]=0;
     }
+
     long long o=N;
 
     for(int j=2,m=1;j*j<=N;j++,m++){
         if(o%j==0){
-            DzielnikiPF[m]=1;
             Dzielniki[m]=j;
         }
         while(o%j==0){
@@ -42,10 +41,16 @@ int main()
             Wielokrotnosci[m]++;
         }
     }
-    for(int i=0;i*i<=N;i++){
-        if(DzielnikiPF[i]==1){
-            cout<<"wielokrotnosci "<<Wielokrotnosci[i]<<" -dzielnik:"<<Dzielniki[i]<<endl;
+    for(int i=0;i<P;i++){
+        Wielokrotnosci2[i]=Wielokrotnosci[1];
+    }
+    sort (Wielokrotnosci2,Wielokrotnosci2+(P+1));
+    cout<<Wielokrotnosci2[P]<<endl;
+    for(int i=0;i<P;i++){
+        if(Wielokrotnosci[i]==Wielokrotnosci2[P]){
+            cout<<Dzielniki[i]<<endl;
         }
     }
+
 }
 
