635ac7abd8cd639afccd81809663f6d55998dea2
MateuszMateo <k.mateusz120@gmail.com>
2015-12-11 18:28:43 +0100

    Dodanie z powrotem "DzielnikiPF"

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ int main()
     int P=sqrt(N);
     int Wielokrotnosci[P+1];
     int Dzielniki[P+1];
+    int DzielnikiPF[P+1];
     int Wielokrotnosci2[P+1];
 
     for(int i=0;i<=sqrt(N);i++){
@@ -34,6 +35,7 @@ int main()
 
     for(int j=2,m=1;j*j<=N;j++,m++){
         if(o%j==0){
+            DzielnikiPF[m]=1;
             Dzielniki[m]=j;
         }
         while(o%j==0){
@@ -49,6 +51,24 @@ int main()
     for(int i=0;i<P;i++){
         if(Wielokrotnosci[i]==Wielokrotnosci2[P]){
             cout<<Dzielniki[i]<<endl;
+        }
+    }
+    //od tąd to szkic innego rozwiązania, ponieważ to wyżej nie działa
+
+    int D=0;
+    for(int i=0;i*i<=N;i++){
+        if(DzielnikiPF[i]==1){
+            D++;
+        }
+    }
+    int TablicaW[D+1];
+    int TablicaD[D+1];
+    int B=0;
+    for(int i=0;i*i<=N;i++){
+        if(DzielnikiPF[i]==1){
+            TablicaW[B]=Wielokrotnosci[i];
+            TablicaD[B]=Dzielniki[i];
+            B++;
         }
     }
 
